4db310896af22f09b48519bb62cf455050cffb6e
Romaan Tariq <tm23834@gmail.com>
2025-10-19 15:53:50 -0400

    declaring constants  by using DOM

---
+++ lab-2/js/page-color-changer.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    // Declare constants for sliders and html elements
+    const redSlider = document.getElementById('red');
+    const greenSlider = document.getElementById('green');
+    const blueSlider = document.getElementById('blue');
+    const htmlElement = document.querySelector('html');
