4c71f4781ce4bc7b0be65b360361886a0ddc095e
Romaan Tariq <tm23834@gmail.com>
2025-10-19 15:55:57 -0400

    function created to update background colors

--- lab-2/js/page-color-changer.js
+++ lab-2/js/page-color-changer.js
@@ -5,3 +5,15 @@ window.addEventListener('DOMContentLoaded', () => {
     const greenSlider = document.getElementById('green');
     const blueSlider = document.getElementById('blue');
     const htmlElement = document.querySelector('html');
+    
+    //function created to update background colors
+     function updateBackgroundColor() {
+        const red = redSlider.value;
+        const green = greenSlider.value;
+        const blue = blueSlider.value;
+
+        const rgbColor = `rgb(${red}, ${green}, ${blue})`;
+        htmlElement.style.backgroundColor = rgbColor;
+
+        console.log(`Background color changed to: ${rgbColor}`);
+    }
